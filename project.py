247609204e948be1117496ad2d1b5ae3fcf9ac37
Carlos Corti <carlosacorti@hotmail.com>
2020-03-04 16:05:12 +0100

    Restaurant menu list
    Query restaurantmenu.db for the first restaurant and show the menu list

--- project.py
+++ project.py
@@ -1,29 +1,38 @@
 from flask import Flask
-
-#creo un objeto Flask con la aplicacion
 app = Flask(__name__)
 
-#los decoradores modifican la funcion basica debajo, 
-#en este caso .route se apliaca a app e incluye HelloWorlo dentro de app.route
-#setean la url del argumento del decorador y luego se ejecuta "def HelloWorld()"
-#entonces cada vez que la url es "host port 5000/" o "host port 5000/hello"
-#se ejecuta def HelloWorld() y devuelve el string "Hello World" en pantalla
-#ademas no hace falta que ejecute la respuesta del servidor al cliente
-@app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World"
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
-#si esta aplicacion es invocada por el interperete de python ejecuta app.run
-#si es importada como modulo de otra aplicacion no (__name__ != __main__)
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/menu')
+def RestoItem():
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+
+    output = ""
+    output = "<h1>%s Menu</h1>" % restaurant.name
+    output += '</br>'
+    output += '<ol>'
+    for i in items:
+        output += '<li>'
+        output += '<h3>%s</h3>' % i.name
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br></br>'
+        output += '</li>'
+    output += '</ol>'
+
+    return output
+
 if __name__ == '__main__': 
-    
-    #el servidor se refresca cada vez que hay un cambio de codigo
-    #no hace falta reiniciar el servidor cada vez que hago un cambio en el codigo
-    #y provee un debugger en el navegador
     app.debug = True
-    
-    #corre el local server, por default solo es accesible desde host machine
-    #host = '0.0.0.0' explicita que puedo escuchar cualquier ip, osea es de acceso publico
-    #port = 5000 es el puerto en la maquina host en que se va a
     app.run(host = '0.0.0.0', port= 5000) 
