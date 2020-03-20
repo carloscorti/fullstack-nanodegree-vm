3c175493b610eaa73281b81592a3b067af4f0ad5
Carlos Corti <carlosacorti@hotmail.com>
2020-03-20 16:45:23 +0100

    adding html templates

--- project.py
+++ project.py
@@ -25,28 +25,14 @@ def RestoItem(restaurant_id=3):
     resto = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = resto.id)
     
-    output = ""
-    output = "<h1>%s Menu</h1>" % resto.name
-    output += '</br>'
-    output += '<ol>'
-    for i in items:
-        output += '<li>'
-        output += '<h3>%s</h3>' % i.name
-        output += i.price
-        output += '</br>'
-        output += i.description
-        output += '</br></br>'
-        output += '</li>'
-    output += '</ol>'
-    
-    return output
+    return render_template('menulist.html', resto=resto, items=items)
 
 # Task 1: Create route for newMenuItem function here
 @app.route('/restaurant/<int:restaurant_id>/new/')
 def newMenuItem(restaurant_id):
     resto = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    output = "<h1>%s | Add Menu Item</h1>" % resto.name
-    return "page to create a new menu item. Task 1 complete!</br></br>" + output
+    
+    return render_template('newmenuitem.html', resto=resto)
 
 # Task 2: Create route for editMenuItem function here
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
@@ -54,8 +40,10 @@ def editMenuItem(restaurant_id, menu_id):
     resto = session.query(Restaurant.name).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem.name).filter_by(restaurant_id = restaurant_id).values(MenuItem.name)
     item_list = list(items)
-    output = "<h1>%s | Total Menu Items %s <br> Edit Menu Item %s</h1>" % (resto.name, len(item_list), item_list[menu_id].name)
-    return "page to edit a menu item. Task 2 complete!" + output
+    item_list_len = len(item_list)
+
+    return render_template('editmenuitem.html', resto=resto, item_list=item_list, menu_id=menu_id, item_list_len=item_list_len)
+    
 
 # Task 3: Create a route for deleteMenuItem function here
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
@@ -63,8 +51,10 @@ def deleteMenuItem(restaurant_id, menu_id):
     resto = session.query(Restaurant.name).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem.name).filter_by(restaurant_id = restaurant_id).values(MenuItem.name)
     item_list = list(items)
-    output = "<h1>%s | Total Menu Items %s <br> Delete Menu Item %s</h1>" % (resto.name, len(item_list), item_list[menu_id].name)
-    return "page to delete a menu item. Task 3 complete!" + output
+    item_list_len = len(item_list)
+
+    return render_template('deletemenuitem.html', resto=resto, item_list=item_list, menu_id=menu_id, item_list_len=item_list_len)
+
 
 if __name__ == '__main__': 
     app.debug = True
