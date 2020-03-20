9b7a555cc83f85694028fe6474df09e9bcb8c691
Carlos Corti <carlosacorti@hotmail.com>
2020-03-20 20:10:36 +0100

    add delete and edit to each menu item
    *delete and edit added anchorsd in menulist.html template
    *added return anchor in deletemenuitem.html an editmenuitem.html
    to its relative menu items list

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template         
+from flask import Flask, render_template, url_for       
 app = Flask(__name__)
 
 from sqlalchemy import create_engine, func
@@ -19,13 +19,13 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
-@app.route('/')
 @app.route('/restaurant/<int:restaurant_id>/')
-def RestoItem(restaurant_id=3):
+def restoMenuItem(restaurant_id=3):
     resto = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = resto.id)
-    
+
     return render_template('menulist.html', resto=resto, items=items)
+
 
 # Task 1: Create route for newMenuItem function here
 @app.route('/restaurant/<int:restaurant_id>/new/')
@@ -37,7 +37,7 @@ def newMenuItem(restaurant_id):
 # Task 2: Create route for editMenuItem function here
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
-    resto = session.query(Restaurant.name).filter_by(id = restaurant_id).one()
+    resto = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem.name).filter_by(restaurant_id = restaurant_id).values(MenuItem.name)
     item_list = list(items)
     item_list_len = len(item_list)
@@ -48,7 +48,7 @@ def editMenuItem(restaurant_id, menu_id):
 # Task 3: Create a route for deleteMenuItem function here
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
-    resto = session.query(Restaurant.name).filter_by(id = restaurant_id).one()
+    resto = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem.name).filter_by(restaurant_id = restaurant_id).values(MenuItem.name)
     item_list = list(items)
     item_list_len = len(item_list)
