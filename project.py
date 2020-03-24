5edc6c18498ff73c93861bb71030848bb1b48936
Carlos Corti <carlosacorti@hotmail.com>
2020-03-24 18:08:51 +0100

    add css

--- project.py
+++ project.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, url_for, request, redirect      
+from flask import Flask, render_template, url_for, request, redirect, flash      
 app = Flask(__name__)
 
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
-#from webserver import pathChunk
 
 #By default, check_same_thread is True and only the creating thread may use the connection.
 # If set False, the returned connection may be shared across multiple threads. 
@@ -36,6 +35,7 @@ def newMenuItem(restaurant_id):
         newItem = MenuItem(name=request.form['newItem'], restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
+        flash('New Menu Item %s was created!!' % newItem.name)
         return redirect(url_for('restoMenuItem', restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html', resto=resto, restaurant_id=restaurant_id)
@@ -46,15 +46,18 @@ def newMenuItem(restaurant_id):
 def editMenuItem(restaurant_id, menu_id):
     resto = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem.name).filter_by(restaurant_id = restaurant_id).values(MenuItem.name)
+    items_id = session.query(MenuItem.id).filter_by(restaurant_id = restaurant_id).values(MenuItem.id)
     item_list = list(items)
+    item_id_list = list(items_id)
     item_list_len = len(item_list)
 
     if request.method == 'POST':
         if request.form['editItem']:
-            itemToEdit = session.query(MenuItem).filter_by(restaurant_id = restaurant_id, name=item_list[menu_id].name).one()
+            itemToEdit = session.query(MenuItem).filter_by(restaurant_id = restaurant_id, name=item_list[menu_id].name, id=item_id_list[menu_id].id).one()
             itemToEdit.name = request.form['editItem']
             session.add(itemToEdit)
             session.commit()
+            flash('The Item %s was Edited to %s!!' % (item_list[menu_id].name, itemToEdit.name))
         return redirect(url_for('restoMenuItem', restaurant_id=restaurant_id))
     else:
         return render_template('editmenuitem.html', resto=resto, item_list=item_list, menu_id=menu_id, item_list_len=item_list_len)
@@ -65,19 +68,23 @@ def editMenuItem(restaurant_id, menu_id):
 def deleteMenuItem(restaurant_id, menu_id):
     resto = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem.name).filter_by(restaurant_id = restaurant_id).values(MenuItem.name)
+    items_id = session.query(MenuItem.id).filter_by(restaurant_id = restaurant_id).values(MenuItem.id)
     item_list = list(items)
+    item_id_list = list(items_id)
     item_list_len = len(item_list)
 
     if request.method == 'POST':
         deleteItem = item_list[menu_id].name
-        itemToDelete = session.query(MenuItem).filter_by(name = deleteItem).one()
+        itemToDelete = session.query(MenuItem).filter_by(name = deleteItem, id=item_id_list[menu_id].id).one()
         session.delete(itemToDelete)
         session.commit()
+        flash('The Item %s was Deleted!!' % deleteItem)
         return redirect(url_for('restoMenuItem', restaurant_id=restaurant_id))
     else:
         return render_template('deletemenuitem.html', resto=resto, item_list=item_list, menu_id=menu_id, item_list_len=item_list_len)
 
 
 if __name__ == '__main__': 
+    app.secret_key = 'secret_key' #flask lo usa para crear sesiones para los usuarios tiene que ser un passwoerd seguro
     app.debug = True
     app.run(host = '0.0.0.0', port= 5000) 
